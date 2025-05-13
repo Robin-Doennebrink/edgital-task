1e88a0f52b7ab662590dc8422ef0e4383d72b927
Robin Dönnebrink <doennebrink77@googlemail.com>
2025-05-13 13:11:42 +0200

    feat: Add a model directory which holds the model RoadNetwork

---
+++ src/models/base.py
@@ -0,0 +1,11 @@
+"""
+Description: The declarative base class for using SQLAlchemy
+
+Author: Robin Dönnebrink
+Created: 13.05.2025
+Copyright: © 2025 Robin Dönnebrink
+"""
+from sqlalchemy.orm import DeclarativeBase
+
+class Base(DeclarativeBase):
+    pass
---
+++ src/models/road_network.py
@@ -0,0 +1,19 @@
+"""
+Description: Class to model a road network.
+
+Author: Robin Dönnebrink
+Created: 13.05.2025
+Copyright: © 2025 Robin Dönnebrink
+"""
+
+from sqlalchemy import  String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from models.base import Base
+
+class RoadNetwork(Base):
+    __tablename__  = "road_network"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    owner: Mapped[str] = mapped_column(String)
+
